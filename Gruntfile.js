3fdc1c0ff8d3e12cdfd25103b4bfc41e5aec682c
athaax <jonathansolis@dhcp80ff3112.dynamic.uiowa.edu>
2014-06-06 13:55:40 -0500

    working out cab division updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,33 +6,37 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
     globalConfig: globalConfig,
     pkg: grunt.file.readJSON('package.json'),
+    
+    //compile the sass
 
     sass: {
       dist: { 
         files: {
-          '<%=globalConfig.themeDir %>/css/app.css' : '<%=globalConfig.themeDir %>/scss/app.scss'
+          '<%=globalConfig.themeDir %>/css/master.css' : '<%=globalConfig.themeDir %>/scss/master.scss'
         },                  // Target
         options: {              // Target options
           style: 'compressed',
           sourcemap: 'true',
-          loadPath: ['<%=globalConfig.themeDir %>/bower_components/foundation/scss']
+          loadPath: ['division-project/scss']
         }
       }
     },
+
     //concat all the files into the build folder
 
     concat: {
       js:{
-        src: [
-          '<%=globalConfig.themeDir %>/bower_components/modernizr/modernizr.js',
-          '<%=globalConfig.themeDir %>/bower_components/foundation/js/foundation.min.js',
-          '<%=globalConfig.themeDir %>/bower_components/FlexSlider/jquery.flexslider.js',
+        src: ['division-project/bower_components/jquery/jquery.js',
+          'division-project/bower_components/jquery.equalheights/jquery.equalheights.js',
+          'division-project/bower_components/fitvids/jquery.fitvids.js',
+          'division-project/bower_components/flexslider/jquery.flexslider.js',
           'division-bar/js/division-bar.js',
-          '<%=globalConfig.themeDir %>/javascript/*.js'
-        ],
-        dest: '<%=globalConfig.themeDir %>/build/src/main_concat.js'
+          '<%=globalConfig.themeDir %>/js/*.js', 
+          'division-project/js/*.js'],
+        dest: '<%=globalConfig.themeDir %>/build/build-src.js'
       }
     },
 
@@ -40,13 +44,15 @@ module.exports = function(grunt) {
     //toggle mangle to leave variable names intact
 
     uglify: {
+      options: {
+        mangle: true
+      },
       my_target:{
         files:{
-        '<%=globalConfig.themeDir %>/build/build.js': ['<%=globalConfig.themeDir %>/build/src/main_concat.js'],
+        '<%=globalConfig.themeDir %>/build/build.js': ['<%=globalConfig.themeDir %>/build/build-src.js'],
         }
       }
     },
-
     watch: {
       scripts: {
         files: ['<%=globalConfig.themeDir %>/js/*.js', '<%=globalConfig.themeDir %>/js/**/*.js'],
@@ -56,9 +62,7 @@ module.exports = function(grunt) {
         }
       },
       css: {
-        files: ['<%=globalConfig.themeDir %>/scss/*.scss', 
-                '<%=globalConfig.themeDir %>/scss/**/*.scss'
-                ],
+        files: ['<%=globalConfig.themeDir %>/scss/*.scss', '<%=globalConfig.themeDir %>/scss/**/*.scss', 'division-project/scss/*.scss','division-project/scss/**/*.scss'],
         tasks: ['sass'],
         options: {
           spawn: true,
@@ -72,8 +76,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-watch');
 
   // Default task(s).
   // Note: order of tasks is very important
