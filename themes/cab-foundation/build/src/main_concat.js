4083729e29eb57d8066c55f9c920166add6619ca
Dustin Quam <quamsta@gmail.com>
2014-09-09 11:22:50 -0500

    fix for tile spacing

--- themes/cab-foundation/build/src/main_concat.js
+++ themes/cab-foundation/build/src/main_concat.js
@@ -1463,41 +1463,41 @@ $(document).foundation();
     });
   });
 // all map styles
-var styles = [	 
-	{
-	    "stylers": [
-	      { "weight": 2.7 },
-	      { "saturation": -71 },
-	      { "invert_lightness": true },
-	      { "visibility": "on" },
-	      { "hue": "#8800ff" }
-	    ]
-	  },{
-	    "elementType": "geometry",
-	    "stylers": [
-	      { "weight": 1.6 },
-	      { "lightness": -20 },
-	      { "saturation": -13 },
-	      { "gamma": 1.33 }
-	    ]
-	  },{
-	    "featureType": "road",
-	    "elementType": "geometry.fill",
-	    "stylers": [
-	      { "color": "#000000" },
-	      { "weight": 3.6 },
-	      { "hue": "#a200ff" },
-	      { "lightness": 38 },
-	      { "saturation": -4 }
-	    ]
-	  },{
-	    "featureType": "water",
-	    "stylers": [
-	      { "hue": "#00b2ff" },
-	      { "lightness": -2 },
-	      { "saturation": 27 }
-	    ]
-	  }
+var styles = [
+{
+	"stylers": [
+	{ "weight": 2.7 },
+	{ "saturation": -71 },
+	{ "invert_lightness": true },
+	{ "visibility": "on" },
+	{ "hue": "#8800ff" }
+	]
+},{
+	"elementType": "geometry",
+	"stylers": [
+	{ "weight": 1.6 },
+	{ "lightness": -20 },
+	{ "saturation": -13 },
+	{ "gamma": 1.33 }
+	]
+},{
+	"featureType": "road",
+	"elementType": "geometry.fill",
+	"stylers": [
+	{ "color": "#000000" },
+	{ "weight": 3.6 },
+	{ "hue": "#a200ff" },
+	{ "lightness": 38 },
+	{ "saturation": -4 }
+	]
+},{
+	"featureType": "water",
+	"stylers": [
+	{ "hue": "#00b2ff" },
+	{ "lightness": -2 },
+	{ "saturation": 27 }
+	]
+}
 ];
 function GetLocation(address) {
 	var geocoder = new google.maps.Geocoder();
