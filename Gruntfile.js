b6d17a2516e5f9d98379721a4e4c4310eabaa60a
Dustin Quam <dustinquam@me.com>
2015-01-15 11:46:45 -0600

    updated SS and foundation

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
           'division-bar/js/division-bar.js',
           '<%=globalConfig.themeDir %>/javascript/*.js'
         ],
-        dest: '<%=globalConfig.themeDir %>/build/src/main_concat.js'
+        dest: '<%=globalConfig.themeDir %>/build/build.src.js'
       }
     },
 
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
     uglify: {
       my_target:{
         files:{
-        '<%=globalConfig.themeDir %>/build/build.js': ['<%=globalConfig.themeDir %>/build/src/main_concat.js'],
+        '<%=globalConfig.themeDir %>/build/build.js': ['<%=globalConfig.themeDir %>/build/build.src.js'],
         }
       }
     },
