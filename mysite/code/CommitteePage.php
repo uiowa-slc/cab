83a5cd240918d9b85e7c1c0938ea5daa026b64ec
Dustin Quam <quamsta@gmail.com>
2020-11-11 10:00:47 -0600

    pattern fix

--- mysite/code/CommitteePage.php
+++ mysite/code/CommitteePage.php
@@ -2,8 +2,6 @@
 
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Assets\Image;
-use SilverStripe\Control\Director;
-use SilverStripe\ORM\FieldType\DBHTMLText;
 
 class CommitteePage extends Page {
 
@@ -24,26 +22,12 @@ class CommitteePage extends Page {
 	private static $has_many = array(
 
 	);
-	private static $icon_class = 'font-icon-torsos-all';
+
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
 		$fields->addFieldToTab('Root.Main', new UploadField('Image', 'Committee Image'), 'Content');
 
 		return $fields;
-	}
-	public function RenderedImage() {
-
-		$absolutePath = Director::baseFolder();
-		$name = $this->URLSegment;
-		$imagePath = $absolutePath . '/themes/cab/dist/images/committees/' . $name . '.png';
-		$image = new DBHTMLText();
-		if (file_exists($imagePath)) {
-			$image->setValue('<img class="d-block w-100" alt="" role="presentation" src="_resources/themes/cab/dist/images/committees/' . $name . '.png" />');
-			return $image;
-		} else {
-			return false;
-		}
-
 	}
 
 }
--- themes/bootstrap/templates/SilverStripe/UserForms/Model/Layout/UserDefinedForm.ss
+++ themes/bootstrap/templates/SilverStripe/UserForms/Model/Layout/UserDefinedForm.ss
@@ -2,13 +2,13 @@
 
 <main class="container-xl my-5">
 	<div class="row">
-		<div class="<% if $Menu(2) %>col-lg-8 col-xl-9 <% else %>col-md-10 offset-md-1<% end_if %>">
+		<div class="<% if $Menu(2) %>col-lg-8 col-xl-9 <% else %>col-md-9 offset-md-2<% end_if %>">
             
 			<article id="content">
 
 			<h1>$Title</h1>
             $Content
-            $Form
+        	$Form
 			$PageComments
 			</article>
 	
